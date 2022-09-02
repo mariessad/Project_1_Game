72ff3e37ccf43bc0a2d11fe17d00e2f549c6b632
mariessad <74148632+mariessad@users.noreply.github.com>
2022-09-02 14:20:58 -0400

    add some starter JS

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // Bubble pop two player game
+// put entire game inside button start function so the game starts on press
+
+const canvas = document.getElementById("canvas-1");
+
+// "2d", leading to the creation of a CanvasRenderingContext2D
+//object representing a two-dimensional rendering context.
+
+const context = canvas.getContext("2d");
+
+// canvas height and width have to be the same here as CSS
+
+canvas.width = 800;
+canvas.hight = 500;
+
+// initialze score for both players
+let score1 = 0;
+let score2 = 0;
+
+let gameFrame = 0;
+
+// mouse object ability to use the mouse to play game
+
+const mouse = {
+  // gives a cross axis by dividing the canvas
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+};
